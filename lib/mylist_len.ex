425825e46ce1e03e6e3f55473c603bef440e2966
droidsam1 <droidsam@gmail.com>
2025-12-07 12:47:28 +0100

    simplify multiclause

--- lib/mylist_len.ex
+++ lib/mylist_len.ex
@@ -1,11 +1,8 @@
 # Create a function list_len/1 that calculates the length of a list
 defmodule MyList do
-  def list_len([]) do
-    0
-  end
 
-  def list_len([_head | tail]) do
-    list_len(1, tail)
+  def list_len(list) do
+    list_len(0, list)
   end
 
   defp list_len(accumulator, []) do
