bd07ea07dae5411f29e11f0ef12678b587075319
droidsam1 <droidsam@gmail.com>
2025-12-08 09:41:16 +0100

    add extract method

--- test/large_lines_test.exs
+++ test/large_lines_test.exs
@@ -2,6 +2,11 @@ defmodule LargeLinesTest do
   use ExUnit.Case
 
   test "large_lenghts/1 returns line lengths" do
+    path = create_temp_file()
+    assert LargeLines.large_lenghts(path) == [5, 23, 79]
+  end
+
+  defp create_temp_file() do
     path =
       System.tmp_dir!()
       |> Path.join("large_lines_test_#{System.unique_integer([:positive])}.txt")
@@ -13,6 +18,6 @@ defmodule LargeLinesTest do
       "short\nthis line is exactly 23\nthis one is much longer than eighty characters so it should be counted properly\n"
     )
 
-    assert LargeLines.large_lenghts(path) == [5, 23, 79]
+    path
   end
 end
