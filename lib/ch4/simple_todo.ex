9037eb718e1607bf1b854812d73f65fd0fc10cc0
Samuel <samuel.taboas@cabify.com>
2026-01-02 18:08:47 +0100

    define type definition and fix spec signature

--- lib/ch4/simple_todo.ex
+++ lib/ch4/simple_todo.ex
@@ -3,19 +3,20 @@ defmodule SimpleTodo do
     Simple Module to store and retrieve values, using simple raw functional approach (no agents, no genservers yet)
   """
   defstruct entries: %{}
+  @type t :: %SimpleTodo{entries: %{}}
 
   def new(initial_state \\ %{}) do
     %SimpleTodo{entries: initial_state}
   end
 
-  @spec add(map(), Date.t(), String.t()) :: SimpleTodo
+  @spec add(SimpleTodo.t(), Date.t(), String.t()) :: SimpleTodo.t()
   def add(todo, day, new_task) do
     tasks_for_date = Map.get(todo.entries, day) || []
     tasks_for_date = [new_task | tasks_for_date]
     %SimpleTodo{entries: Map.put(todo.entries, day, tasks_for_date)}
   end
 
-  @spec get(map(), Date.t()) :: map()
+  @spec get(SimpleTodo.t(), Date.t()) :: list()
   def get(todo, day) do
     Map.get(todo.entries, day, [])
   end
