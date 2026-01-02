ba7e50b75772fc13a9be929fe577d6e30ae01e72
Samuel <samuel.taboas@cabify.com>
2026-01-02 18:02:13 +0100

    refactor to use internal struct

--- lib/ch4/simple_todo.ex
+++ lib/ch4/simple_todo.ex
@@ -2,20 +2,21 @@ defmodule SimpleTodo do
   @moduledoc """
     Simple Module to store and retrieve values, using simple raw functional approach (no agents, no genservers yet)
   """
+  defstruct entries: %{}
 
   def new(initial_state \\ %{}) do
-    initial_state
+    %SimpleTodo{entries: initial_state}
   end
 
-  @spec add(map(), Date.t(), String.t()) :: map()
-  def add(initial_state, day, new_task) do
-    tasks_for_date = Map.get(initial_state, day) || []
+  @spec add(map(), Date.t(), String.t()) :: SimpleTodo
+  def add(todo, day, new_task) do
+    tasks_for_date = Map.get(todo.entries, day) || []
     tasks_for_date = [new_task | tasks_for_date]
-    Map.put(initial_state, day, tasks_for_date)
+    %SimpleTodo{entries: Map.put(todo.entries, day, tasks_for_date)}
   end
 
   @spec get(map(), Date.t()) :: map()
-  def get(initial_state, day) do
-    Map.get(initial_state, day, [])
+  def get(todo, day) do
+    Map.get(todo.entries, day, [])
   end
 end
--- test/ch4/simple_todo_test.exs
+++ test/ch4/simple_todo_test.exs
@@ -3,28 +3,29 @@ defmodule SimpleTodoTest do
   use ExUnit.Case
 
   test "new returns an empty todo" do
-    assert SimpleTodo.new() == %{}
+    assert %SimpleTodo{entries: %{}} = SimpleTodo.new()
   end
 
   test "add puts some taks in the todo" do
     day = Date.utc_today()
     another_day = Date.add(day, 1)
-    initial_state = SimpleTodo.new()
-    state_after_first = SimpleTodo.add(initial_state, day, "buy some bread")
+    state_after_first = SimpleTodo.add(SimpleTodo.new(), day, "buy some bread")
 
-    assert state_after_first == %{day => ["buy some bread"]}
+    assert state_after_first == %SimpleTodo{entries: %{day => ["buy some bread"]}}
 
-    assert SimpleTodo.add(state_after_first, another_day, "buy some milk") == %{
-             day => ["buy some bread"],
-             another_day => ["buy some milk"]
+    assert SimpleTodo.add(state_after_first, another_day, "buy some milk") == %SimpleTodo{
+             entries: %{
+               day => ["buy some bread"],
+               another_day => ["buy some milk"]
+             }
            }
   end
 
   test "get returns and specific task from the todo" do
     day = Date.utc_today()
-    initial_state = SimpleTodo.new(%{day => ["remember the milk"]})
+    todo = SimpleTodo.new(%{day => ["remember the milk"]})
 
-    assert SimpleTodo.get(initial_state, day) == ["remember the milk"]
+    assert SimpleTodo.get(todo, day) == ["remember the milk"]
   end
 
   test "add can put n number of tasks in the same date" do
