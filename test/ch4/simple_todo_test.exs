8ae75f80f8e514168939f14ba9063c52bb42aae4
Samuel <samuel.taboas@cabify.com>
2026-01-02 15:15:54 +0100

    fix format

--- test/ch4/simple_todo_test.exs
+++ test/ch4/simple_todo_test.exs
@@ -12,8 +12,8 @@ defmodule SimpleTodoTest do
     initial_state = SimpleTodo.new()
     state_after_first = SimpleTodo.add(initial_state, day, "buy some bread")
 
-
     assert state_after_first == %{day => ["buy some bread"]}
+
     assert SimpleTodo.add(state_after_first, another_day, "buy some milk") == %{
              day => ["buy some bread"],
              another_day => ["buy some milk"]
@@ -33,6 +33,7 @@ defmodule SimpleTodoTest do
     todo = SimpleTodo.add(todo, day, "remember the milk")
     todo = SimpleTodo.add(todo, day, "remember the vegetables")
 
-    assert Enum.sort(SimpleTodo.get(todo, day)) == Enum.sort(["remember the milk", "remember the vegetables"])
+    assert Enum.sort(SimpleTodo.get(todo, day)) ==
+             Enum.sort(["remember the milk", "remember the vegetables"])
   end
 end
