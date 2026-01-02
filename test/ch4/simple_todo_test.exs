12da7959925c909cf7a2af7ee74f7d7de23e0f44
Samuel <samuel.taboas@cabify.com>
2026-01-02 14:47:11 +0100

    refactor to use date as key

--- test/ch4/simple_todo_test.exs
+++ test/ch4/simple_todo_test.exs
@@ -7,19 +7,22 @@ defmodule SimpleTodoTest do
   end
 
   test "add puts some value in the todo" do
+    day = Date.utc_today()
+    another_day = Date.add(day, 1)
     initial_state = SimpleTodo.new()
-    state_after_first = SimpleTodo.add(initial_state, 1, "buy some bread")
-    assert state_after_first == %{1 => "buy some bread"}
+    state_after_first = SimpleTodo.add(initial_state, day, "buy some bread")
+    assert state_after_first == %{day => "buy some bread"}
 
-    assert SimpleTodo.add(state_after_first, 2, "buy some milk") == %{
-             1 => "buy some bread",
-             2 => "buy some milk"
+    assert SimpleTodo.add(state_after_first, another_day, "buy some milk") == %{
+             day => "buy some bread",
+             another_day => "buy some milk"
            }
   end
 
   test "get returns and specific task from the todo" do
-    initial_state = SimpleTodo.new(%{1 => "remember the milk"})
+    day = Date.utc_today()
+    initial_state = SimpleTodo.new(%{day => "remember the milk"})
 
-    assert SimpleTodo.get(initial_state, 1) == "remember the milk"
+    assert SimpleTodo.get(initial_state, day) == "remember the milk"
   end
 end
