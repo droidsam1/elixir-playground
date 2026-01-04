d45d4d7296fd13ce1f488a1ab3c39182ea631083
Samuel <samuel.taboas@cabify.com>
2026-01-04 09:01:21 +0100

    rewrite update from scratch just for practice

--- test/ch4/simple_todo_test.exs
+++ test/ch4/simple_todo_test.exs
@@ -47,19 +47,18 @@ defmodule SimpleTodoTest do
 
   test "update an specific entry" do
     day = Date.utc_today()
+    another_day = Date.add(day, 1)
     todo = SimpleTodo.new()
+    todo = SimpleTodo.add(todo, another_day, "remember the flour")
     todo = SimpleTodo.add(todo, day, "remember the milk")
     todo = SimpleTodo.add(todo, day, "remember the vegetables")
     todo = SimpleTodo.add(todo, day, "remember the fruit")
 
+    todo = SimpleTodo.update(todo, day, 4, "remember the eggs")
 
-    todo = SimpleTodo.update(todo, day, 3, "remember the eggs")
+    expected_values = ["remember the milk", "remember the vegetables", "remember the eggs"]
+    actual_values = Enum.map(SimpleTodo.get(todo, day), & &1.value)
 
-
-    assert Enum.sort([
-      %SimpleTodoEntry{id: 1, value: "remember the milk"},
-      %SimpleTodoEntry{id: 2, value: "remember the vegetables"},
-      %SimpleTodoEntry{id: 3, value: "remember the eggs"}
-    ]) == Enum.sort(SimpleTodo.get(todo, day))
+    assert Enum.sort(expected_values) == Enum.sort(actual_values)
   end
 end
