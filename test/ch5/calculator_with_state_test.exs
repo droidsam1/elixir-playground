3b0ffaf2b4840a3910ff60a3f2c65f7a0e972da6
Samuel <samuel.taboas@cabify.com>
2026-01-17 19:38:40 +0100

    add simple addition op

--- test/ch5/calculator_with_state_test.exs
+++ test/ch5/calculator_with_state_test.exs
@@ -1,8 +1,13 @@
 defmodule CalculatorWithStateTest do
   use ExUnit.Case
 
-  test "perfom sum two numbers" do
+  test "start/0 starts a new proces" do
     pid = CalculatorServer.start()
     assert pid != nil && is_pid(pid)
+  end
+
+  test "add sums the number to server state" do
+    pid = CalculatorServer.start()
+    assert {:add, 10} = CalculatorServer.add(pid, 10)
   end
 end
