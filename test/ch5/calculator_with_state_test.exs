f89c8c1e124ce0e9d35328e9dd0c2bbc251b5eff
Samuel <samuel.taboas@cabify.com>
2026-01-17 19:40:37 +0100

    add op to return the current value

--- test/ch5/calculator_with_state_test.exs
+++ test/ch5/calculator_with_state_test.exs
@@ -6,8 +6,13 @@ defmodule CalculatorWithStateTest do
     assert pid != nil && is_pid(pid)
   end
 
-  test "add sums the number to server state" do
+  test "add sums the number to server accumulator" do
     pid = CalculatorServer.start()
     assert {:add, 10} = CalculatorServer.add(pid, 10)
+  end
+
+  test "start inits the internal accumulator to zero" do
+    pid = CalculatorServer.start()
+    assert 0 = CalculatorServer.value(pid)
   end
 end
