53b5e3b6c2a11a1fb943cfb222ca18589241926b
Samuel <samuel.taboas@cabify.com>
2026-01-24 15:21:08 +0100

    validate restart process with unit test

--- test/ch5/database_server_test.exs
+++ test/ch5/database_server_test.exs
@@ -8,8 +8,24 @@ defmodule DatabaseServerTest do
     children = Supervisor.which_children(sup)
 
     assert Enum.any?(children, fn
-      {DatabaseServer, _pid, _type, _modules} -> true
-      _ -> false
-    end)
+             {DatabaseServer, _pid, _type, _modules} -> true
+             _ -> false
+           end)
+  end
+
+  test "restart DatabaseServer when it crashses" do
+    start_supervised!(Ch5.Supervisor)
+
+    DatabaseServer.storage("a test key", "a value")
+    assert "a value" = DatabaseServer.retrieve("a test key")
+
+    old_pid = Process.whereis(DatabaseServer)
+
+    # force a crash in the server
+    DatabaseServer.retrieve(:crash)
+
+    new_pid = Process.whereis(DatabaseServer)
+    assert "a value" = DatabaseServer.retrieve("a test key")
+    assert new_pid != old_pid
   end
 end
