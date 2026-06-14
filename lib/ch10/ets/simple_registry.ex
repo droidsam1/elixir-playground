0844b51391de852993319ec42fbd78cf5b74471e
Samuel <samuel.taboas@cabify.com>
2026-06-14 12:50:41 +0200

    add type and ets

--- lib/ch10/ets/simple_registry.ex
+++ lib/ch10/ets/simple_registry.ex
@@ -2,15 +2,19 @@ defmodule SimpleRegistry do
   @moduledoc "Simple registry to practice with ETS tables"
   use GenServer
 
+  @table_name :simple_registry
+
   def init(init_arg) do
     {:ok, init_arg}
   end
 
   def start_link() do
-    GenServer.start_link(__MODULE__, {})
+    ets_table = :ets.new(@table_name, [:set, :named_table])
+    GenServer.start_link(__MODULE__, ets_table)
   end
 
-  def register(a_name) do
+  @spec register(atom()) :: :ok
+  def register(_a_name) do
     :ok
   end
 end
